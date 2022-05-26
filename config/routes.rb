0af16a75fd1599e9538786f55cb10ab3d0980522
AlexBrown4020 <balex3497@gmail.com>
2022-05-26 13:50:41 +0900

    Refactor: Clean up routes and index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   root to: "activities#index"
   get "/activities", to: "activities#index"
   get "/activities/:id", to: "activities#show"
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
