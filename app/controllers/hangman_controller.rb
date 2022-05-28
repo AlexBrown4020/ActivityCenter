b1275c41a8fc635fbd046c4766a9cc60c28e15f0
AlexBrown4020 <balex3497@gmail.com>
2022-05-28 13:39:00 +0900

    Feat: Attempt to implement function in browser

--- app/controllers/hangman_controller.rb
+++ app/controllers/hangman_controller.rb
@@ -3,17 +3,11 @@ class HangmanController < ApplicationController
 respond_to? :html, :json
 
     def initialize
-        session[:lives] = 7
+        @lives = 7
         @words = ["chrysalis", "zap", "hundred", "ruby", "python", "hippopotamus",]
-        session[:secret_word] = @words.sample
-        session[:board] = [" _ "] * session[:secret_word].size
-        @abc = "New game started    "<< session[:user_string]
-            respond_with @abc
+        @secret_word = @words.sample
+        @board = [" _ "] * @secret_word.length
     end
-
-    # def board
-
-    # end
 
     # def process_letter
     #     @id = params[:id]
@@ -83,18 +77,18 @@ respond_to? :html, :json
     end
 
     # Start function
-    def start
-        puts "Welcome to Hangman"
-        while @lives > 0 && !won?
-            puts "You have #{@lives} lives left. Please make a guess: "
-            # puts board_state
-            guess = make_guess
-            update_board(guess)
-        end
-         if won?
-            puts "You win!"
-         else 
-            puts "You lost!"
-        end
-    end
+    # def start
+    #     puts "Welcome to Hangman"
+    #     while @lives > 0 && !won?
+    #         puts "You have #{@lives} lives left. Please make a guess: "
+    #         # puts board_state
+    #         guess = make_guess
+    #         update_board(guess)
+    #     end
+    #      if won?
+    #         puts "You win!"
+    #      else 
+    #         puts "You lost!"
+    #     end
+    # end
 end
